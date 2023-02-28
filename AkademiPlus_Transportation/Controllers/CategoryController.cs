591ed33311c7771fb76c061e5c638ee5a850ecd3
atakan18 <atakankasbasi@gmail.com>
2023-02-28 23:34:03 +0300

    procesd added

--- AkademiPlus_Transportation/Controllers/CategoryController.cs
+++ AkademiPlus_Transportation/Controllers/CategoryController.cs
@@ -16,9 +16,42 @@ namespace AkademiPlus_Transportation.Controllers
             var values = db.TblCategory.ToList();
             return View(values);
         }
+
+        [HttpGet]
         public ActionResult AddCategory()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(TblCategory tblCategory)
+        {
+            db.TblCategory.Add(tblCategory);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var value = db.TblCategory.Find(id);
+            db.TblCategory.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var value = db.TblCategory.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(TblCategory tblCategory)
+        {
+            var value = db.TblCategory.Find(tblCategory.CategoryID);
+            value.CategoryName = tblCategory.CategoryName;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
