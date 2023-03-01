48551d7357fd4d2720bd6ebd546562efe58cdd47
atakan18 <atakankasbasi@gmail.com>
2023-03-01 23:14:06 +0300

    dropdownlist ve linkleme + crud

---
+++ AkademiPlus_Transportation/Controllers/ProcessController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AkademiPlus_Transportation.Models;
+
+namespace AkademiPlus_Transportation.Controllers
+{
+    public class ProcessController : Controller
+    {
+        // GET: Process
+        DbTransportEntities db = new DbTransportEntities();
+
+        public ActionResult Index()
+        {
+            var values = db.TblProcess.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddProcess()
+        {
+            List<SelectListItem> value1 = (from x in db.TblTransportation.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.TransportationID.ToString(),
+                                               Value = x.TransportationID.ToString()
+                                           }).ToList();
+            ViewBag.v1 = value1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProcess(TblProcess tblProcess)
+        {
+            db.TblProcess.Add(tblProcess);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult DeleteProcess(int id)
+        {
+            var value = db.TblProcess.Find(id);
+            db.TblProcess.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateProcess(int id) 
+        {
+            List<SelectListItem> value1 = (from x in db.TblTransportation.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.TransportationID.ToString(),
+                                               Value = x.TransportationID.ToString()
+                                           }).ToList();
+            ViewBag.v1 = value1;
+            var values = db.TblProcess.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult UpdateProcess(TblProcess tblProcess)
+        {
+            var values = db.TblProcess.Find(tblProcess.ProcessID);
+            values.Transportation = tblProcess.Transportation;
+            values.Description = tblProcess.Description;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AkademiPlus_Transportation/Controllers/TransportationController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AkademiPlus_Transportation.Models;
+
+namespace AkademiPlus_Transportation.Controllers
+{
+    public class TransportationController : Controller
+    {
+        // GET: Transportation
+        DbTransportEntities db = new DbTransportEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblTransportation.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddTransportation()
+        {
+            List<SelectListItem> value1 = (from x in db.TblCustomer.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = x.CustomerName + " " + x.CustomerSurname,
+                                              Value = x.CustomerID.ToString()
+                                          }).ToList();
+
+            List<SelectListItem> value2 = (from x in db.TblCategory.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = x.CategoryName,
+                                              Value = x.CategoryID.ToString()
+                                          }).ToList();
+
+            List<SelectListItem> value3 = (from x in db.TblProduct.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.ProductName,
+                                               Value = x.ProductID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> value4 = (from x in db.TblEmployee.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.EmployeeName,
+                                               Value = x.EmployeeID.ToString()
+                                           }).ToList();
+            ViewBag.v1 = value1;
+            ViewBag.v2 = value2;
+            ViewBag.v3 = value3;
+            ViewBag.v4 = value4;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddTransportation(TblTransportation tblTransportation)
+        {
+            tblTransportation.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TblTransportation.Add(tblTransportation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteTransportation(int id)
+        {
+            var value = db.TblTransportation.Find(id);
+            db.TblTransportation.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateTransportation(int id)
+        {
+            List<SelectListItem> value1 = (from x in db.TblCustomer.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CustomerName + " " + x.CustomerSurname,
+                                               Value = x.CustomerID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> value2 = (from x in db.TblCategory.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> value3 = (from x in db.TblProduct.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.ProductName,
+                                               Value = x.ProductID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> value4 = (from x in db.TblEmployee.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.EmployeeName,
+                                               Value = x.EmployeeID.ToString()
+                                           }).ToList();
+            ViewBag.v1 = value1;
+            ViewBag.v2 = value2;
+            ViewBag.v3 = value3;
+            ViewBag.v4 = value4;
+            var value = db.TblTransportation.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateTransportation(TblTransportation tblTransportation)
+        {
+            var value = db.TblTransportation.Find(tblTransportation.TransportationID);
+            value.Customer = tblTransportation.Customer;
+            value.Category = tblTransportation.Category;
+            value.Product = tblTransportation.Product;
+            value.Employee = tblTransportation.Employee;
+            value.Status = tblTransportation.Status;
+            value.Departure = tblTransportation.Departure;
+            value.Arrival = tblTransportation.Arrival;
+            value.Date = tblTransportation.Date;
+            value.Description = tblTransportation.Description;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
