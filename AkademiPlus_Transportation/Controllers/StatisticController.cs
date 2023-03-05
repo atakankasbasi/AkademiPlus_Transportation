3899add7bd91c7b45e0233aa0eac3f6c161f3027
atakan18 <atakankasbasi@gmail.com>
2023-03-06 00:31:16 +0300

    linq query

--- AkademiPlus_Transportation/Controllers/StatisticController.cs
+++ AkademiPlus_Transportation/Controllers/StatisticController.cs
@@ -15,7 +15,10 @@ namespace AkademiPlus_Transportation.Controllers
             ViewBag.customerCount = db.TblCustomer.Count();
             ViewBag.cityAnkara = db.TblCustomer.Where(x => x.CustomerCity == "Ankara").Count();
             ViewBag.categoryCount = db.TblCategory.Count();
-            ViewBag.citySelect = db.TblCustomer.Where(x => x.CustomerID == 1).Select(y => y.CustomerCity);
+            //ViewBag.citySelect = db.TblCustomer.Where(x => x.CustomerID == 1).Select(y => y.CustomerCity);
+            ViewBag.employeeName = db.TblEmployee.Where(x => x.EmployeeID == 1).Select(y => y.EmployeeName).SingleOrDefault();
+            ViewBag.urunSayısı = db.TblProduct.Count();
+            ViewBag.cityName = db.TblCustomer.Where(x => x.CustomerID == 1).Select(y => y.CustomerCity).SingleOrDefault();
             return View();
         }
     }
